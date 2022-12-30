425fe8a75db9532c63756e4678eafe2c4574290c
Justin Hornsby <justin.hornsby@entelect.co.za>
2022-12-30 14:43:23 +0200

    implement navigation with event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipe-book-project';
+  loadedFeature = 'recipe';
+
+  onNavigate(featureDestination: string) {
+    this.loadedFeature = featureDestination;
+  }
 }
